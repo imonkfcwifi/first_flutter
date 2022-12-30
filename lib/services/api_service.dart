977206862770087faf0daf3692364e076e491085
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-30 18:39:25 +0900

    models and new api service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:flu/models/webtoon.dart';
+import 'package:flu/models/webtoon_detail_model.dart';
+import 'package:flu/models/webtoon_episode_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
@@ -19,6 +21,33 @@ class ApiService {
         toonsList.add(WebtoonModel.fromJson(webtoon));
       }
       return toonsList;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLastestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstance = [];
+
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstance.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstance;
     }
     throw Error();
   }
