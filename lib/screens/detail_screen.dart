e9f0d262eb4e561cfa2c9ef71b6f73919fe4b0ce
imonkfcwifi <nohyungkorea@gmail.com>
2023-01-02 16:36:40 +0900

    URL
    take me to church

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flu/models/webtoon_detail_model.dart';
 import 'package:flu/models/webtoon_episode_model.dart';
 import 'package:flu/services/api_service.dart';
+import 'package:flu/widgets/episode_widget.dart';
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatefulWidget {
@@ -117,38 +118,9 @@ class _DetailScreenState extends State<DetailScreen> {
                     return Column(
                       children: [
                         for (var episode in snapshot.data!)
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 10),
-                            // inset(padding) 은 element와 테두리 사이의 element 내부에 있는 공간을 의미합니다.
-
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.green.shade400,
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 10,
-                                horizontal: 20,
-                              ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    episode.title,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                  const Icon(
-                                    Icons.chevron_right_rounded,
-                                    color: Colors.white,
-                                  )
-                                ],
-                              ),
-                            ),
+                          Episode(
+                            episode: episode,
+                            webtoonId: widget.id,
                           )
                       ],
                     );
@@ -163,5 +135,6 @@ class _DetailScreenState extends State<DetailScreen> {
         ),
       ),
     );
+    // } https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}
   }
 }
---
+++ lib/widgets/episode_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flu/models/webtoon_episode_model.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class Episode extends StatelessWidget {
+  const Episode({
+    Key? key,
+    required this.episode,
+    required this.webtoonId,
+  }) : super(key: key);
+  final String webtoonId;
+  final WebtoonEpisodeModel episode;
+
+  onButtnTap() async {
+    await launchUrlString(
+      "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}",
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtnTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        // inset(padding) 은 element와 테두리 사이의 element 내부에 있는 공간을 의미합니다.
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.green.shade400,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
